e0c63b717b4555e18aeb53d93f1a08243151761e
Sathwik_S <sathwiks2020@gmail.com>
2021-07-09 12:17:37 +0530

    version 2.0 with responsive

--- food-memory/app.js
+++ food-memory/app.js
@@ -52,17 +52,20 @@ const cardArray = [
 var cardChosen = [];
 var cardChosenId = [];
 var cardsWon = [];
-var resultDisplay = document.querySelector("#result")
+var resultDisplay = document.querySelector(".score-cnt");
 
 const grid = document.querySelector(".grid");
 
-cardArray.sort(() => 0.5 - Math.random())
+cardArray.sort(() => 0.5 - Math.random());
+
+var sa = 0;
 
 function createBoard(){
     for(let i=0; i<cardArray.length; i++){
         var card = document.createElement('img');
         card.setAttribute("src","images/blank.png");
         card.setAttribute("data-id", i);
+        card.setAttribute("class", "card-img");
         card.addEventListener("click", flipcard);
         grid.appendChild(card);
     }
@@ -73,7 +76,7 @@ function checkForMatch(){
     const optionOneId = cardChosenId[0];
     const optionTwoId = cardChosenId[1];
     if(cardChosen[0] == cardChosen[1]){
-        alert("You found a match");
+        // alert("You found a match");
         cards[optionOneId].setAttribute("src", "images/white.png");
         cards[optionTwoId].setAttribute("src", "images/white.png");
         cardsWon.push(cardChosen);
@@ -85,22 +88,33 @@ function checkForMatch(){
     cardChosen=[];
     cardChosenId=[];
     resultDisplay.textContent = cardsWon.length;
+    sa=0;
     if(cardsWon.length === cardArray.length/2){
         resultDisplay.textContent="Congratulations! You found them all!!";
+        document.querySelector(".score-title").textContent="";
+        const ci = document.querySelectorAll(".card-img")
+        for(let i=0; i<ci.length; i++){
+            ci[i].setAttribute("src", cardArray[i].img);
+            // console.log(grid[i]);
+        }
     }
 }
 
 function flipcard(){
-    var cardId = this.getAttribute('data-id');
-    cardChosen.push(cardArray[cardId].name);
-    cardChosenId.push(cardId);
-    this.setAttribute('src', cardArray[cardId].img);
-    // console.log(cardChosen);
-    if(cardChosen.length === 2){
-        setTimeout(checkForMatch, 1000);
-        // checkForMatch();
+    if(sa<2){
+        var cardId = this.getAttribute('data-id');
+        cardChosen.push(cardArray[cardId].name);
+        cardChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
         // console.log(cardChosen);
+        if(cardChosen.length === 2){
+            setTimeout(checkForMatch, 600);
+            // checkForMatch();
+            // console.log(cardChosen);
+        }
+        sa++;
     }
+    
 }
 
 createBoard();
